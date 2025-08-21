f6f3d6e7452e15f94b43c6b921d7e58a61f7b4b6
Manan-0107 <mananshah1127@gmail.com>
2025-08-22 00:22:55 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter the value");
+    scanf("%d",&a);
+    if (a%2==0){
+        printf("the no is even");
+    }
+    else{
+        printf("the no is odd");
+    };
+}
